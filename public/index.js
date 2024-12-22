b549f977239ca6b15c03675d32fa7e8c8f0ce775
unknown <aivaneezydakii@gmail.com>
2024-12-22 21:54:22 +0100

    Completed the getTask

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const handlePostReq = async () => {
   const taskName = $("input[type=text][name=taskName]").val();
-  const completed = $("input[type=checkbox][name=checkbox]:checked").val();
+  const completed = $("input[type=checkbox][name=checkBox]:checked").val();
+
   if (!taskName) {
     return;
   }
@@ -19,23 +20,18 @@ const handleGetAllTask = async () => {
   try {
     const res = await axios.get("/api/v1/tasks");
     const tasks = res.data;
-    const taskList = $("#taskList");
+    const taskList = $("#tasksList");
     tasks.forEach((task) => {
-      taskList.append(
-        $("<li>").append(
-          $("<a>")
-            .attr("href", "/test")
-            .text(`${task.name} - ${task.completed}`)
-        )
-      );
+      const anchor = $("<a>")
+        .attr("href", `task.html?id=${task._id}`)
+        .text(`${task.name} - ${task.completed}`);
+
+      taskList.append($("<li>").append(anchor));
       taskList.append($("<button>").text("Edit"));
       taskList.append($("<button>").text("Delete"));
     });
-
-    console.log(res);
   } catch (err) {
     console.log(err);
   }
 };
-
-$(document).ready(handleGetAllTask());
+$(document).ready(handleGetAllTask);
---
+++ public/task.js
@@ -0,0 +1,14 @@
+const handleGetTask = async (id) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get("id");
+  try {
+    const res = await axios.get(`/api/v1/tasks/${myParam}`);
+    const { name, completed } = res.data;
+    const taskList = $("#taskList");
+    taskList.append($("<li>").text(`${name} - ${completed}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+$(document).ready(handleGetTask);
