d65759299b095e88fd4e7a60027f703f0c6f9d80
unknown <aivaneezydakii@gmail.com>
2024-12-24 13:21:35 +0100

    Finished the project

--- public/index.js
+++ public/index.js
@@ -1,7 +1,47 @@
+const editButton = (id) => {
+  return $("<a>")
+    .attr("href", `edit.html?id=${id}`)
+    .append(
+      $("<button>")
+        .text("Edit")
+        .addClass(" text-center btn btn-success  my-2 w-100")
+    );
+};
+
+const deleteButton = () => {
+  return $("<button>").text("Delete").addClass("btn btn-danger flex-grow-1");
+};
+
+const renderTask = (task) => {
+  console.log("render", task);
+  const taskList = $("#tasksList");
+
+  const anchor = $("<a>")
+    .attr("href", `task.html?id=${task._id}`)
+    .text(`${task.name} - ${task.completed}`);
+
+  const taskItem = $("<li>")
+    .addClass("d-flex flex-column list-group-item")
+    .append(anchor);
+
+  const buttonContainer = $("<div>").addClass(
+    "d-flex flex-row justify-between-center align-items-center w-100"
+  );
+  const editBtn = editButton(task._id);
+  buttonContainer.append(editBtn);
+  taskItem.append(editBtn);
+
+  const deleteBtn = deleteButton();
+  deleteBtn.click(() => deleteTask(task._id, taskItem));
+  buttonContainer.append(deleteBtn);
+  taskItem.append(deleteBtn);
+
+  taskList.append(taskItem);
+};
+
 const handlePostReq = async () => {
   const taskName = $("input[type=text][name=taskName]").val();
   const completed = $("input[type=checkbox][name=checkBox]:checked").val();
-
   if (!taskName) {
     return;
   }
@@ -12,29 +52,7 @@ const handlePostReq = async () => {
       completed: completed ? true : false,
     });
 
-    const task = res.data.task;
-    const taskList = $("#tasksList");
-    // Create a new list item for the task
-    const anchor = $("<a>")
-      .attr("href", `task.html?id=${task._id}`)
-      .text(`${task.name} - ${task.completed}`);
-    const listItem = $("<li>").append(anchor);
-
-    const editButton = $("<a>")
-      .attr("href", `edit.html?id=${task._id}`)
-      .append($("<button>").text("Edit"));
-    listItem.append(editButton);
-
-    const deleteButton = $("<button>").text("Delete");
-    deleteButton.addClass("test");
-    listItem.append(deleteButton);
-
-    // Append the new task to the task list
-    taskList.append(listItem);
-
-    // Attach the task._id to the delete button's click event
-    deleteButton.click(() => deleteTask(task._id));
-
+    renderTask(res.data);
     // Clear the input fields after adding the task
     $("input[type=text][name=taskName]").val("");
     $("input[type=checkbox][name=checkBox]").prop("checked", false);
@@ -52,27 +70,11 @@ const handleGetAllTask = async () => {
   try {
     const res = await axios.get("/api/v1/tasks");
     const tasks = res.data;
-    const taskList = $("#tasksList");
 
+    console.log("ta=ls", tasks);
     tasks.map((task, id) => {
-      const anchor = $("<a>")
-        .attr("href", `task.html?id=${task._id}`)
-        .text(`${task.name} - ${task.completed}`);
-      taskList.append($("<li>").addClass("list-group-item ").append(anchor));
-
-      const editButton = $("<a>")
-        .attr("href", `edit.html?id=${task._id}`)
-        .append(
-          $("<button>").text("Edit").addClass("btn btn-success mx-2 my-2")
-        );
-      taskList.append(editButton);
-
-      const deleteButton = $("<button>").text("Delete");
-      deleteButton.addClass("btn btn-danger");
-      taskList.append(deleteButton);
-
-      // Attach the task._id to the delete button's click event
-      deleteButton.click(() => deleteTask(task._id));
+      console.log("task", task);
+      renderTask(task);
     });
   } catch (err) {
     console.log(err);
@@ -80,9 +82,12 @@ const handleGetAllTask = async () => {
 };
 $(document).ready(handleGetAllTask);
 
-const deleteTask = async (id) => {
+const deleteTask = async (id, taskItem) => {
   try {
     const res = await axios.delete(`/api/v1/tasks/${id}`);
+    if (res.status == 200) {
+      taskItem.remove();
+    }
   } catch (err) {
     console.log(err);
   }
